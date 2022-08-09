e06154c243a9ef986d78e806df842f7146e663c0
Alexandre96dev <alexandre.nascimento96dev@gmail.com>
2022-08-09 12:19:09 -0500

    Adição do model Dog

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,14 +1,36 @@
 import { Request, Response} from "express";
+import * as Dog from '../models/Dog';
 
 export const home = (req:Request,res:Response)=>{
-    res.send('pages/home')
+    res.render('pages/page',{
+        banner:{
+            title: 'Todos os animais',
+            backgroundImage: 'allanimals.jpg'
+        }
+    })
 }
 export const dogs = (req:Request,res:Response)=>{
-    res.send('pages/home')
+    res.render('pages/page',{
+        banner:{
+            title: 'Todos os cachorros',
+            backgroundImage: 'banner_dog.jpg'
+        },
+        photos:Dog.getDogs
+    })
 }
 export const cats = (req:Request,res:Response)=>{
-    res.send('pages/home')
+    res.render('pages/page',{
+        banner:{
+            title: 'Todos os gatos',
+            backgroundImage: 'banner_cat.jpg'
+        }
+    })
 }
 export const fishes = (req:Request,res:Response)=>{
-    res.send('pages/home')
+    res.render('pages/page',{
+        banner:{
+            title: 'Todos os peixes',
+            backgroundImage: 'banner_fish.jpg'
+        }
+    })
 }
---
+++ src/models/Dog.ts
@@ -0,0 +1,11 @@
+export const getDogs = () => {
+    return [
+        {nome: 'golden', cor: "Amarelo"},
+        {nome: 'husky', cor: "Branco e preto"},
+        {nome: 'labrador', cor: "Amarelo"},
+        {nome: "pastor-alemao", cor: "Amarelo e Preto"},
+        {nome: "poodle", cor: "branco"},
+        {nome: 'zwergspitz', cor: "Amarelo"}
+        
+    ]
+}
