46117a496e251e55d156e76f6276a342a3b43917
Alexandre96dev <alexandre.nascimento96dev@gmail.com>
2022-12-27 17:23:23 -0300

    Alterações projeto

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,36 +1,48 @@
 import { Request, Response} from "express";
-import * as Dog from '../models/Dog';
+import { Pet } from "../models/Pet";
+import { createMenuObject } from "../helpers/createMenuObject";
 
 export const home = (req:Request,res:Response)=>{
+    let list = Pet.getAll();
     res.render('pages/page',{
+        menu: createMenuObject('all'),
         banner:{
             title: 'Todos os animais',
             backgroundImage: 'allanimals.jpg'
-        }
+        },
+        list      
     })
 }
 export const dogs = (req:Request,res:Response)=>{
+    let list = Pet.getFromType('dog');
     res.render('pages/page',{
+        menu: createMenuObject('dog'),
         banner:{
             title: 'Todos os cachorros',
             backgroundImage: 'banner_dog.jpg'
         },
-        photos:Dog.getDogs
+        list 
     })
 }
 export const cats = (req:Request,res:Response)=>{
+    let list = Pet.getFromType('cat');
     res.render('pages/page',{
+        menu: createMenuObject('cat'),
         banner:{
             title: 'Todos os gatos',
             backgroundImage: 'banner_cat.jpg'
-        }
+        },
+        list 
     })
 }
 export const fishes = (req:Request,res:Response)=>{
+    let list = Pet.getFromType('fish');
     res.render('pages/page',{
+        menu: createMenuObject('fish'),
         banner:{
             title: 'Todos os peixes',
             backgroundImage: 'banner_fish.jpg'
-        }
+        },
+        list 
     })
 }
--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,5 +1,10 @@
 import { Request, Response} from "express";
+import { Pet } from "../models/Pet";
+import { createMenuObject } from "../helpers/createMenuObject";
 
 export const search = (req:Request,res:Response)=>{
-    res.send('pages/home')
+    res.render('pages/page', {
+        menu: createMenuObject(''),
+        
+    })    
 }
